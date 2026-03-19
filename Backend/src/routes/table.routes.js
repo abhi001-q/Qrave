bd6f8ccda2cb0a28c88935f4852cdfa567ce1e94
abhi001-q <abhishek987ff@gmail.com>
2026-03-19 17:36:42 +0545

    feat(backend): define Table management API routes

--- Backend/src/routes/table.routes.js
+++ Backend/src/routes/table.routes.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
-// TODO: implement table routes
+const table = require("../controllers/table.controller");
+
+router.get("/", table.getAll);
+router.get("/available", table.getAvailable);
+
 module.exports = router;
